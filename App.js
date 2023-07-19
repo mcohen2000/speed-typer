8e8de09e50679103862da3e1205d9f8a5d770217
Michael Cohen <macball2000@yahoo.com>
2023-07-19 16:06:05 -0400

    Timer + Cancel Typing After Error

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TextInput, KeyboardAvoidingView } from 'react-native';
 
 export default function App() {
   const [input, setInput] = useState('');
-  const testTexts = ['The quick brown fox jumped over the lazy dog.']
+  const testTexts = ['The quick brown fox jumps over the lazy dog.']
   const [currentTest, setCurrentTest] = useState(testTexts[0]);
+  const [startTime, setStartTime] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  function handleStart(newText){
+    if(!isActive && !isDone){
+      setStartTime(Date.now());
+      setIsActive(true);
+    }
+    if (input.length=== 1 && newText === ''){
+      setInput('');
+    }
+    if (newText.length >= 1 && newText.slice(0, -1) === currentTest.slice(0, newText.length-1)){
+      setInput(newText);
+    }
+  }
+  useEffect(() => {
+    let interval = null;
+    if (isActive && isDone===false){
+      interval = setInterval(() => {
+        setTimer((Date.now() - startTime)/1000)
+      }, 100);
+    };
+    if(input === currentTest){
+      setIsActive(false);
+      setIsDone(true);
+      clearInterval(interval);
+    };
+    return () => {
+      clearInterval(interval);
+    };
+  }, [input, isActive])
+
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Speed Typer</Text>
       <View style={styles.testWrapper}>
         <View style={styles.results}>
+          <Text style={styles.timer}>{timer.toFixed(1)} s</Text>
           <Text style={styles.testText}>{currentTest}</Text>
           <Text style={styles.test}>{[...input].map((char, index) => 
             (char === currentTest[index] ?
@@ -25,8 +60,8 @@ export default function App() {
               style={styles.input}
               placeholder='Start typing to begin!'
               placeholderTextColor='white'
-              onChangeText={ newText => setInput(newText)}
-              defaultValue={input}
+              onChangeText={newText => handleStart(newText)}
+              value={input}
               />
 
           </KeyboardAvoidingView>
@@ -63,6 +98,11 @@ const styles = StyleSheet.create({
     color: 'white',
     paddingBottom: 8,
   },
+  timer: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 32,
+  },
   results: {},
   test: {
   },
@@ -73,6 +113,7 @@ const styles = StyleSheet.create({
     margin: 16,
   },
   input: {
+    textAlign: 'center',
     color: 'white',
     marginTop: 8,
     padding: 8,
